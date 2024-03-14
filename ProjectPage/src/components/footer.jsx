6b628a3e2557133c87518afb101e7862a57d52d4
Nikita Thomas <nikitathomas@Nikitas-MBP.WiFi Extender>
2024-03-13 20:30:16 -0400

    About Me Page

---
+++ ProjectPage/src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <a href="https://github.com/Nikitaaa95">GitHub</a>
+        <a href="https://www.linkedin.com/in/nikita-thomas-34b9a153/">LinkedIn</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
