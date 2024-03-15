e6a52b8909fb89165eb5bd70c6e0e0082972f0d7
Nikita Thomas <nikitathomas@Nikitas-MBP.lan>
2024-03-15 11:57:30 -0400

    debug react basics

--- ProjectPage/src/components/footer.jsx
+++ ProjectPage/src/components/footer.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
-      <div>
-        <a href="https://github.com/Nikitaaa95">GitHub</a>
-        <a href="https://www.linkedin.com/in/nikita-thomas-34b9a153/">LinkedIn</a>
-      </div>
+      <nav>
+        <div>
+          <a href="https://github.com/Nikitaaa95">GitHub</a>
+          <a href="https://www.linkedin.com/in/nikita-thomas-34b9a153/">LinkedIn</a>
+        </div>
+      </nav>
     </footer>
   );
 }
--- ProjectPage/src/components/portfolio.jsx
+++ ProjectPage/src/components/portfolio.jsx
@@ -5,14 +5,15 @@ function Portfolio() {
     <section id="portfolio">
       <h2>Portfolio</h2>
       <div className="project">
-        <img src="path-to-project-image.jpg" alt="Project Name" />
         <div className='Project1'>
+          <img src="path-to-project-image.jpg" alt="Project Name" />
           <h3>Project Placeholder</h3>
           <p>Description space</p>
           <a href="project-link">View App</a>
           <a href="git-hub-link">GitHub Repo</a>
         </div>
         <div className='Project2'>
+          <img src="path-to-project-image.jpg" alt="Project Name" />
           <h3>Password Generator</h3>
           <p>Description Space</p>
           <a href="project-link">View App</a>
